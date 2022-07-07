0b084b95b1e50c9b3bb7b0ae5052ce939536d9f9
christophe126 <90414556+christophe126@users.noreply.github.com>
2022-07-07 21:42:08 +0200

    mobil menu

--- js/script.js
+++ js/script.js
@@ -1,7 +1,11 @@
+const btn = document.getElementById('menu-btn')
+const menu = document.getElementById('menu')
+
 const input = document.getElementById('link-input')
 const linkForm = document.getElementById('link-form')
 const errMsg = document.getElementById('err-msg')
 
+btn.addEventListener('click', navToggle)
 linkForm.addEventListener('submit', formsubmit)
 
 // Validate a URL
@@ -18,7 +22,14 @@ function validURL(str) {
   return !!pattern.test(str)
 }
 
+// Toggle Mobile Menu
+function navToggle() {
+  btn.classList.toggle('open')
+  menu.classList.toggle('flex')
+  menu.classList.toggle('hidden')
+}
 
+// Validate a URL
 function formsubmit(e) {
   e.preventDefault()
 
